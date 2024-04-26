e0f1dcb9a2f47a349c13d41fe161dc494c3166f5
RochilleNabong <nabongrochille@gmail.com>
2024-04-26 08:25:51 +0800

    Done

--- event-driven-programming-2C/config/database.php
+++ event-driven-programming-2C/config/database.php
@@ -1,19 +1,17 @@
 <?php 
-// Database Credentials
+
 $servername = "localhost";
 $username = "root";
-$password = ""; // Default Password: Blank | Empty
+$password = ""; 
 
-$database_name = "db_event_driven_programming";
+$database_name = "db_tes";
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $database_name);
 
-// Check connection
+
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
-//echo "Connected successfully";
 
 ?>
